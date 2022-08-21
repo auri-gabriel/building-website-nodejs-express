4e565b8a4f70365baffa8b07e15d2b3266e0c0d7
Auri Gabriel <ririmello.a@gmail.com>
2022-08-21 03:27:13 -0300

    ch. 3

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+const router = express.Router();
+
+module.exports = (params) => {
+  router.get('/', (req, res) => {
+    // Visit Count Cookie
+    // if (!req.session.visitcount) {
+    //   req.session.visitcount = 0;
+    // }
+    // req.session.visitcount += 1;
+    // console.log(`Number of visits: ${req.session.visitcount}`);
+
+    res.render('pages/index', { pageTitle: 'Welcome' });
+  });
+
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
+
+  return router;
+};
--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
+
+const routes = require('./routes');
 
 const app = express();
 
 const port = 3000;
+
+// Setting needed for deploy
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['asdu89sua', 'lkasfjk0'],
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakerService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Express Server listening on port ${port}`);
