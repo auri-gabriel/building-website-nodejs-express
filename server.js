501578bf61622922dff2f8776e224ae4611d4470
Auri Gabriel <ririmello.a@gmail.com>
2022-08-21 02:27:44 -0300

    ch. 1

---
+++ server.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Express Server listening on port ${port}`);
+});
